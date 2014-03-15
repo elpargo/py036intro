50c34c3a401e168cd0f0a8fdd6df2c7bd2b22cb5
Jorge Vargas <jorge.vargas@gmail.com>
2014-03-15 04:22:50 -0400

    Another step towards having proper model

--- simple_polls/simple_polls/models.py
+++ simple_polls/simple_polls/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 
+#Not recommended in real production code.
+#because you mix presentation (choices in spanish)
+#also because you need to recompile every time options change
+RATING_CHOICES = (
+    ('5', 'Sali un pro'),
+    ('4', 'Lo entendi casi todo'),
+    ('3', 'Entendi alguito'),
+    ('2', 'Me perdi cuando hablo chino'),
+    ('1', 'No entendi nada, lo solte en banda!'),
+)
+
 class Survey(models.Model):
-	fullname = models.CharField(20)
+	fullname = models.CharField(max_length=20)
  	email = models.EmailField()
-	rating = models.CharField(choices=[1,2,3,4,5])
- 	other_python_interests = models.CharField(500)
+	rating = models.CharField(max_length=1,choices=RATING_CHOICES)
+ 	other_python_interests = models.CharField(max_length=500)
  	pyladies_interest = models.BooleanField()
  	coworking_interest = models.BooleanField()
+ 	satisfied_with_the_talk = models.BooleanField()
