066faa089bd67cf1df269dbb275b56a84ca50a64
Jorge Vargas <jorge.vargas@gmail.com>
2014-03-15 02:33:36 -0400

    Adding a simple path

--- simple_polls/simple_polls/views.py
+++ simple_polls/simple_polls/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def hello(request):
     return HttpResponse("Hello, world.")
+
+def hello_name(request, question_id):
+    return HttpResponse("Hello, {}".format(question_id))
