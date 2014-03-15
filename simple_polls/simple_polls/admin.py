73f0e8eea6b7cfb895d38ed905c6ba283d59dd18
Jorge Vargas <jorge.vargas@gmail.com>
2014-03-15 05:05:27 -0400

    Adding Admin goodies

--- simple_polls/simple_polls/admin.py
+++ simple_polls/simple_polls/admin.py
@@ -4,5 +4,10 @@ from simple_polls.models import Survey
 #admin.site.register(Survey)
 
 class SurveyAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('fullname', 'email','rating','other_python_interests','pyladies_interest','coworking_interest','satisfied_with_the_talk')
+    search_fields = ('fullname', 'email')
+    #list_filter = ('fullname', 'email','rating','other_python_interests','pyladies_interest','coworking_interest','satisfied_with_the_talk')
+    list_filter = ('rating','other_python_interests','pyladies_interest','coworking_interest','satisfied_with_the_talk')
+    readonly_fields = ('email',)
+
 admin.site.register(Survey, SurveyAdmin)
